19e4ff8b26b98ef18b1cfc00e820cb79b5e94691
vamshikrishnadn <vamshikrishnadn573@gmail.com>
2021-09-14 02:31:12 +0530

    changing axios base url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
-export const fetchPosts = () => axios.get('/posts');
-export const addPost = value => axios.post(`/posts?search=${value}`);
-export const fetchSinglePost = id => axios.get(`/posts/${id}`);
+const API = axios.create({
+  baseURL: 'https://dnvk-vocab.herokuapp.com',
+});
+
+export const fetchPosts = () => API.get('/posts');
+export const addPost = value => API.post(`/posts?search=${value}`);
+export const fetchSinglePost = id => API.get(`/posts/${id}`);
